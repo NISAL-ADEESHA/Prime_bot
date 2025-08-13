8c10d5cd8bac08834bac068527dd1bcc7e1bc3aa
Nisal Adeesha <96944088+NISAL-ADEESHA@users.noreply.github.com>
2025-08-13 09:07:55 +0530

    Update vless.js

--- vless.js
+++ vless.js
@@ -3,27 +3,27 @@ const fs = require('fs');
 module.exports = async (client, msg) => {
     const body = msg.body.trim();
 
-    if (body === '.v2ray') {
+    if (body === '.ss') {
         await client.sendMessage(
             msg.from,
 `Please select an option:
-1. මොකක්ද v2ray එකක් කියන්නෙ
+1. මොකක්ද v2ray කියන්නෙ
 2. v2ray එක setup කරන විදිය
-3. v2ray එක ගන්න
+3. v2ray configs ගන්න
 4. Premium
 5. developer`
         );
     } else if (body === '1') {
-        const info = fs.readFileSync('./v2ray-info.txt', 'utf8');
+        const info = fs.readFileSync('./ss-info.txt', 'utf8');
         await client.sendMessage(msg.from, info);
     } else if (body === '2') {
-        const setup = fs.readFileSync('./v2ray-setup.txt', 'utf8');
+        const setup = fs.readFileSync('./ss-setup.txt', 'utf8');
         await client.sendMessage(msg.from, setup);
     } else if (body === '3') {
-        const getV2ray = fs.readFileSync('./v2ray-get.txt', 'utf8');
-        await client.sendMessage(msg.from, getV2ray);
+        const configs = fs.readFileSync('./ss-configs.txt', 'utf8');
+        await client.sendMessage(msg.from, configs);
     } else if (body === '4') {
-        const premium = fs.readFileSync('./v2ray-premium.txt', 'utf8');
+        const premium = fs.readFileSync('./ss-premium.txt', 'utf8');
         await client.sendMessage(msg.from, premium);
     } else if (body === '5') {
         const dev = fs.readFileSync('./developer.txt', 'utf8');
